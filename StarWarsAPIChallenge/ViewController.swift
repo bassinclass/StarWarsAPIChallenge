ae43d9019305db6ca14494f5ed33ebfe8205351e
joe fleischauer <jfleischauer@rlas-116.org>
2019-01-07 10:08:21 -0600

    change to pulling array of characters

--- StarWarsAPIChallenge/ViewController.swift
+++ StarWarsAPIChallenge/ViewController.swift
@@ -7,65 +7,74 @@
 //
 
 import UIKit
+import CommonCrypto
 
 //struct PeopleDescription: Decodable {
 //    let count: Int
 //    let results: [People]
-//}
+//
 
-struct People: Decodable {
+struct People: Codable {
+    let results: [Results]
+}
+
+struct Results: Codable {
     let name: String
-    let height: String
-    let mass: String
-    let gender: String
-    let hair_color: String
-//    let homeworld: String?
-    let eye_color: String
+}
+
+enum CodingKeys: String, CodingKey {
+    case name
 }
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var characterLabel: UILabel!
 
-    var name = String()
-    var height = String()
-    var mass = String()
+    var nameArray: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        parse()
+        
     }
     
     @IBAction func onFindButtonTapped(_ sender: UIButton) {
-        let randomNumber = Int(arc4random_uniform(88))
-        parse(number: randomNumber)
+//        let randomNumber = Int(arc4random_uniform(88))
+        
     }
     
-    func parse(number: Int) {
-        let jsonUrlString = "https://swapi.co/api/people/\(number)/"
+    func parse() {
+        let jsonUrlString = "https://swapi.co/api/people/" // /number is the endpoint for specific characters.
         guard let url = URL(string: jsonUrlString) else { return }
         URLSession.shared.dataTask(with: url) { (data, response, err) in
             guard let data = data else { return }
             do {
-                let people = try JSONDecoder().decode(People.self, from: data)
-                let character = people.name
-                let height = people.height
-                let weight = people.mass
-                let gender = people.gender
-                let hairColor = people.hair_color
-                let eyeColor = people.eye_color
-                self.displayInfo(characterName: character, characterHeight: height, characterWeight: weight, characterGender: gender, characterHair: hairColor, characterEyeColor: eyeColor)
-                print("name: \(character)\nheight: \(height)\nweight: \(weight)")
+                let people = try? JSONDecoder().decode(People.self, from: data)
+//                print(people.results)
+                let names = people!.results
+//                print(names)
+                for name in names {
+                    self.nameArray.append(name.name)
+                }
+                self.nameInfo(array: self.nameArray)
+                
             } catch let jsonErr {
                 print("Error serializing json:", jsonErr)
             }
         }.resume()
     }
 
-    func displayInfo(characterName: String, characterHeight: String, characterWeight: String, characterGender: String, characterHair: String, characterEyeColor: String) {
+    func nameInfo(array:[String])  ->  [String] {
         DispatchQueue.main.async {
-            self.characterLabel.text = "Name: \(characterName)\nHeight: \(characterHeight)\nWeight: \(characterWeight)\nGender: \(characterGender)\nHair Color: \(characterHair)\nEye Color: \(characterEyeColor)"
+           self.nameArray.append(contentsOf: array)
         }
+        return nameArray
     }
     
+ 
+    
 }
+
+
 
