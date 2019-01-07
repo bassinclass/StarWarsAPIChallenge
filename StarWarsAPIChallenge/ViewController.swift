33a50703ae4d8547fa8c82406b1fdc2578b64ab9
joe fleischauer <jfleischauer@rlas-116.org>
2019-01-07 10:35:18 -0600

    app loads data from the array to the tableView.

--- StarWarsAPIChallenge/ViewController.swift
+++ StarWarsAPIChallenge/ViewController.swift
@@ -26,21 +26,25 @@ enum CodingKeys: String, CodingKey {
     case name
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var characterLabel: UILabel!
-
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     var nameArray: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         parse()
+        //print(nameArray) //does not work here yet.
         
     }
     
     @IBAction func onFindButtonTapped(_ sender: UIButton) {
-//        let randomNumber = Int(arc4random_uniform(88))
+        //        let randomNumber = Int(arc4random_uniform(88))
         
     }
     
@@ -51,28 +55,41 @@ class ViewController: UIViewController {
             guard let data = data else { return }
             do {
                 let people = try? JSONDecoder().decode(People.self, from: data)
-//                print(people.results)
-                let names = people!.results
-//                print(names)
-                for name in names {
-                    self.nameArray.append(name.name)
+                //                print(people.results)
+                    let names = people!.results
+                    //                print(names)
+                    for name in names {
+                        self.nameArray.append(name.name)
+                    }
+//                    print(self.nameArray) // prints all of the names
+                DispatchQueue.main.async {
+                     self.tableView.reloadData()
                 }
-                self.nameInfo(array: self.nameArray)
-                
+
             } catch let jsonErr {
                 print("Error serializing json:", jsonErr)
             }
-        }.resume()
+            }.resume()
     }
-
-    func nameInfo(array:[String])  ->  [String] {
+    
+    func nameInfo()  ->  [String] {
         DispatchQueue.main.async {
-           self.nameArray.append(contentsOf: array)
+            self.nameArray.append(contentsOf: self.nameArray)
         }
         return nameArray
     }
     
- 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = nameArray[indexPath.row]
+//        print(nameArray)
+        return cell!
+    }
+    
     
 }
 
