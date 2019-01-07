a3e621530bacdf8823cb232223f2dea7b5183d4d
joe fleischauer <jfleischauer@rlas-116.org>
2019-01-07 10:45:38 -0600

    code challenge comments are in place, ready to be fixed by students.

--- StarWarsAPIChallenge/ViewController.swift
+++ StarWarsAPIChallenge/ViewController.swift
@@ -9,11 +9,6 @@
 import UIKit
 import CommonCrypto
 
-//struct PeopleDescription: Decodable {
-//    let count: Int
-//    let results: [People]
-//
-
 struct People: Codable {
     let results: [Results]
 }
@@ -27,25 +22,14 @@ enum CodingKeys: String, CodingKey {
 }
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    
-    
+
     @IBOutlet weak var tableView: UITableView!
-    
-    
+
     var nameArray: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         parse()
-        //print(nameArray) //does not work here yet.
-        
-    }
-    
-    @IBAction func onFindButtonTapped(_ sender: UIButton) {
-        //        let randomNumber = Int(arc4random_uniform(88))
-        
     }
     
     func parse() {
@@ -55,39 +39,32 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             guard let data = data else { return }
             do {
                 let people = try? JSONDecoder().decode(People.self, from: data)
-                //                print(people.results)
-                    let names = people!.results
-                    //                print(names)
-                    for name in names {
-                        self.nameArray.append(name.name)
-                    }
-//                    print(self.nameArray) // prints all of the names
-                DispatchQueue.main.async {
-                     self.tableView.reloadData()
+                let names = people!.results
+                for name in names {
+                    self.nameArray.append(name.name)
                 }
-
+                //                    print(self.nameArray) // prints all of the names
+                DispatchQueue.main.async {
+//type code to reload tableView data on next line //**********************************************************************************
+                    
+                }
+                
             } catch let jsonErr {
                 print("Error serializing json:", jsonErr)
             }
-            }.resume()
-    }
-    
-    func nameInfo()  ->  [String] {
-        DispatchQueue.main.async {
-            self.nameArray.append(contentsOf: self.nameArray)
-        }
-        return nameArray
+        }.resume()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return nameArray.count
+//type code to set NUMBER OF ROWS on next line//**********************************************************************************
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-        cell?.textLabel?.text = nameArray[indexPath.row]
-//        print(nameArray)
-        return cell!
+//type code to display what is in each row of the tableView on next 3 lines  //****************************************************************
+        
+        
+        
     }
     
     
